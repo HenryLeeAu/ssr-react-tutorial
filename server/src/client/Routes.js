00b3039870f8bda97cb1058d1cd8f8280194fbc2
Henry Lee <contact@henrylee.io>
2018-08-09 16:23:17 +1000

    setup redux

--- server/src/client/Routes.js
+++ server/src/client/Routes.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
-import Home from './components/Home'
 
-export default () =>{
+import Home from './components/Home'
+import UsersList from './components/UsersList'
+
+export default [
+  {
+    path:'/',
+    component:Home,
+    exact:true
+  },
+  {
+    path:'/users',
+    component:UsersList
+  }
+
+]/*() =>{
   return(
     <div>
       <Route exact path="/" component = { Home } />
-   
+      <Route  path="/users" component = { UsersList } />
     </div>
   )
-}
+}*/
--- server/src/index.js
+++ server/src/index.js
@@ -1,14 +1,17 @@
-
+import 'babel-polyfill'
 import express from 'express';
 import renderer from './helpers/renderer'
+import createStore from './helpers/createStore'
 const app = express();
 
 //assign where the public files located
 app.use(express.static('public'));
 
 app.get('*', (req,res)=>{
+  const store = createStore();
 
-  res.send(renderer(req))
+  // some logic to initialize and load data into store
+  res.send(renderer(req,store))
 })
 
 
