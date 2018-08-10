8644542d55bd7ec395bc9508483e152adb52c693
Henry Lee <contact@henrylee.io>
2018-08-10 14:27:24 +1000

    add redux dev tool

--- server/src/client/client.js
+++ server/src/client/client.js
@@ -3,16 +3,17 @@ import 'babel-polyfill'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware,compose  } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import { renderRoutes } from 'react-router-config'
 import Routes from './Routes';
 import reducers from './reducers'
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   reducers,
   window.INITIAL_STATE, 
-  applyMiddleware(thunk)
+  composeEnhancers(applyMiddleware(thunk))
 )
 
 ReactDOM.hydrate(
